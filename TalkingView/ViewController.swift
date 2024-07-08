cc2d6596c164ac9bc07a353dddeec22a780e002a
Lydia <LydiaLu8822@gmail.com>
2024-07-08 16:08:50 +0800

    Initial Commit

---
+++ TalkingView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkingView
+//
+//  Created by Lydia Lu on 2024/7/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
